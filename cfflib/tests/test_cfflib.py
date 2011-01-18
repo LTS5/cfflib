c2e1504e355e75d2714c8129f74f2785675d741e
UbuntuEPFL <cwis@ubuntu.(none)>
2011-01-18 12:58:47 +0100

    Change to use the getters

--- cfflib/tests/test_cfflib.py
+++ cfflib/tests/test_cfflib.py
@@ -16,12 +16,12 @@ def test_connectome():
 
     c = connectome()
     assert_not_equal(c.connectome_meta, None)
-    assert_equal(c.get_connectome_meta().name, 'myconnectome')
-    assert_equal(c.get_connectome_meta().generator, 'cfflib')
-    assert_equal(c.get_connectome_meta().version, '2.0')
+    assert_equal(c.get_connectome_meta().get_name(), 'myconnectome')
+    assert_equal(c.get_connectome_meta().get_generator(), 'cfflib')
+    assert_equal(c.get_connectome_meta().get_version(), '2.0')
     
     c = connectome('Test connectome')
-    assert_equal(c.get_connectome_meta().name, 'Test connectome')
+    assert_equal(c.get_connectome_meta().get_name(), 'Test connectome')
     assert_equal(c.get_all(), [])
     assert_true(c.hasContent_())
     
@@ -50,10 +50,10 @@ def test_cnetwork_nxgraph():
     c = connectome()
     
     n = CNetwork()
-    assert_equal(n.name, 'mynetwork')
+    assert_equal(n.get_name(), 'mynetwork')
     
     n = CNetwork('Test network')
-    assert_equal(n.name, 'Test network')
+    assert_equal(n.get_name(), 'Test network')
     
     g = nx.Graph()
     g.add_node(0)
@@ -64,10 +64,10 @@ def test_cnetwork_nxgraph():
     g.add_edge(0,2)
     
     n.set_with_nxgraph(g)
-    assert_equal(n.name, 'Test network')
-    assert_equal(n.src, 'CNetwork/test_network.gpickle')
-    assert_equal(n.dtype, 'AttributeNetwork')
-    assert_equal(n.fileformat, 'NXGPickle')
+    assert_equal(n.get_name(), 'Test network')
+    assert_equal(n.get_src(), 'CNetwork/test_network.gpickle')
+    assert_equal(n.get_dtype(), 'AttributeNetwork')
+    assert_equal(n.get_fileformat(), 'NXGPickle')
     
     c.add_connectome_network(n)
     assert_not_equal(c.get_connectome_network(), [])
@@ -78,7 +78,7 @@ def test_cnetwork_nxgraph():
     assert_equal(n.get_name(), 'modif')
 
     c.add_connectome_network_from_nxgraph('2nd nxgraph', g)
-    assert_equal(c.get_connectome_network()[1].name, '2nd nxgraph')
+    assert_equal(c.get_connectome_network()[1].get_name(), '2nd nxgraph')
 
 # With graphml
 def test_cnetwork_graphml():
@@ -86,15 +86,15 @@ def test_cnetwork_graphml():
     c = connectome()
     
     n = CNetwork.create_from_graphml('GraphML net', 'data/Networks/network_res83.graphml')
-    assert_equal(n.name, 'GraphML net')
-    assert_equal(n.fileformat, 'GraphML')
+    assert_equal(n.get_name(), 'GraphML net')
+    assert_equal(n.get_fileformat(), 'GraphML')
     c.add_connectome_network(n)
     assert_not_equal(c.get_connectome_network(), [])
-    assert_equal(c.get_connectome_network()[0].src, 'CNetwork/graphml_net.graphml')
+    assert_equal(c.get_connectome_network()[0].get_src(), 'CNetwork/graphml_net.graphml')
     
     c.add_connectome_network_from_graphml('2nd graphml', 'data/Networks/network_res83.graphml')
     assert_equal(len(c.get_connectome_network()), 2)
-    assert_equal(c.get_connectome_network()[1].name, '2nd graphml')
+    assert_equal(c.get_connectome_network()[1].get_name(), '2nd graphml')
 
 # Test the description and metadata
 def test_desc_meta():
@@ -107,12 +107,12 @@ def test_desc_meta():
 def test_save_load():
 
     c = connectome('Load & Save connectome')
-    assert_equal(c.get_connectome_meta().name, 'Load & Save connectome')
-    assert_equal(c.get_connectome_meta().generator, 'cfflib')
-    assert_equal(c.get_connectome_meta().version, '2.0')
+    assert_equal(c.get_connectome_meta().get_name(), 'Load & Save connectome')
+    assert_equal(c.get_connectome_meta().get_generator(), 'cfflib')
+    assert_equal(c.get_connectome_meta().get_version(), '2.0')
     
     c.add_connectome_network_from_graphml('GraphML net', 'data/Networks/network_res83.graphml')
-    assert_equal(c.get_connectome_network()[0].src, 'CNetwork/graphml_net.graphml')
+    assert_equal(c.get_connectome_network()[0].get_src(), 'CNetwork/graphml_net.graphml')
     n = c.get_connectome_network()[0]
     n.update_metadata({'nb':123})
     assert_true(c.get_connectome_network()[0].get_metadata_as_dict().has_key('nb'))
@@ -120,10 +120,10 @@ def test_save_load():
     save_to_cff(c, op.join(TMP, 'test.cff'))
     
     c2 = load_from_cff(op.join(TMP, 'test.cff'))
-    assert_equal(c2.get_connectome_meta().name, 'Load & Save connectome')
-    assert_equal(c2.get_connectome_meta().generator, 'cfflib')
-    assert_equal(c2.get_connectome_meta().version, '2.0')
-    assert_equal(c2.get_connectome_network()[0].src, 'CNetwork/graphml_net.graphml')
+    assert_equal(c2.get_connectome_meta().get_name(), 'Load & Save connectome')
+    assert_equal(c2.get_connectome_meta().get_generator(), 'cfflib')
+    assert_equal(c2.get_connectome_meta().get_version(), '2.0')
+    assert_equal(c2.get_connectome_network()[0].get_src(), 'CNetwork/graphml_net.graphml')
     assert_true(c2.get_connectome_network()[0].get_metadata_as_dict().has_key('nb'))
 
 #def test_save_nx2cff():
