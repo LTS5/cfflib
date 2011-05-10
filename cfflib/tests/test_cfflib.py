9a123aefecaf41a35b28df0c87489b77cd48f3d0
Stephan Gerhard <git@github.com>
2011-05-10 13:40:00 +0200

    Removed load of nifti in test

--- cfflib/tests/test_cfflib.py
+++ cfflib/tests/test_cfflib.py
@@ -260,9 +260,6 @@ def test_cvolume_nifti1():
     assert_not_equal(c.get_connectome_volume(), [])
     assert_equal(len(c.get_connectome_volume()), 1)
 
-    # Check save/load the CVolume
-    v.load()
-    
     # Check to save the connectome
     save_to_cff(c, op.join(TMP, 'cv.cff'))
     assert_true(op.exists(op.join(TMP, 'cv.cff')))
